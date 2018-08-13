b780b2d27f8dcbf91522be086ded1b15f8ea0e00
sunjx <sunjx1988@163.com>
2018-08-13 17:05:46 +0800

    对集合元素的流式处理

---
+++ src/main/java/jx/sun/feature/stream/StramMain.java
@@ -0,0 +1,42 @@
+package jx.sun.feature.stream;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @Auther: sunjx
+ * @Date: 2018/8/13 0013 16:53
+ * @Description:
+ */
+public class StramMain {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,-2,3,null,34,-5,6,1,21,null,-1);
+        long count = list.stream()
+                .distinct()     //去重
+                .filter(i -> i !=null && i > 0) //过滤
+                .peek(System.out::println)      // 打印
+                .mapToInt(i -> i * 2)           // 每个元素处理
+                .peek(System.out::println)      // 打印
+                .count(); //统计
+
+        /**
+         * 计算结果
+         * 1
+         * 2
+         *
+         * 3
+         * 6
+         *
+         * 34
+         * 68
+         *
+         * 6
+         * 12
+         *
+         * 21
+         * 42
+         */
+    }
+}
