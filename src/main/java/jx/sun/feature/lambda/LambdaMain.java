24ed4c2fe9232826180bb9dda4bc12bfbb199c9f
sunjx <sunjx1988@163.com>
2018-08-13 16:50:08 +0800

    lambda表达式

---
+++ src/main/java/jx/sun/feature/lambda/LambdaMain.java
@@ -0,0 +1,22 @@
+package jx.sun.feature.lambda;
+
+/**
+ * @Auther: sunjx
+ * @Date: 2018/8/13 0013 16:43
+ * @Description:
+ */
+public class LambdaMain {
+
+    public static void main(String[] args) {
+        print(() -> System.out.println("hello"));
+    }
+
+    static void print(PrintAble printAble){
+        printAble.print();
+    }
+}
+
+
+interface PrintAble{
+    void print();
+}
